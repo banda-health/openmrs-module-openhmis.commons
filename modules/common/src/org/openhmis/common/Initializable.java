b49c87710266a076eacf1b9faf3f95f64c45f389
Wes Brown <wes@weslandia.org>
2012-04-20 16:03:37 +0300

    Added documentation to Initializable interface.

--- modules/common/src/org/openhmis/common/Initializable.java
+++ modules/common/src/org/openhmis/common/Initializable.java
@@ -1,5 +1,12 @@
 package org.openhmis.common;
 
+/**
+ * Represents types that must be initialized before being used.
+ */
 public interface Initializable {
+	/**
+	 * Performs the type initialization.
+	 */
 	void initialize();
 }
+
