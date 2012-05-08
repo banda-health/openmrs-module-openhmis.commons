5f819167af84cffd89f84ceb01f52c25dcacd76f
Wes Brown <wes@weslandia.org>
2012-05-08 13:56:01 +0300

    Moved common project from modules to top-level folder.  Removed modules folder as each module will have its own repo.

--- modules/plm/api/src/main/java/org/openmrs/module/plm/ListServiceEventListenerAdapter.java
+++
@@ -1,9 +0,0 @@
-package org.openmrs.module.plm;
-
-public class ListServiceEventListenerAdapter implements ListServiceEventListener {
-	@Override
-	public void listAdded(ListServiceEvent event) { }
-
-	@Override
-	public void listRemoved(ListServiceEvent event) { }
-}
