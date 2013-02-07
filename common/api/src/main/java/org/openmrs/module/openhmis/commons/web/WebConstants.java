91a37375fd41175953d12087ad9850c005eea8fc
Wes Brown <wes@weslandia.org>
2013-02-07 10:53:51 +0300

    Removed old release.properties file, continued work on Constants base, and created Entity diagram.

---
+++ common/api/src/main/java/org/openmrs/module/openhmis/commons/web/WebConstants.java
@@ -0,0 +1,26 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.openhmis.commons.web;
+
+public class WebConstants {
+	public static final String MODULE_BASE = "/module/openhmis/";
+
+	public static String formUrl(String page) {
+		return page.endsWith(".form") ? page : page + ".form";
+	}
+
+	public static String redirectUrl(String page) {
+		return "redirect:" + formUrl(page);
+	}
+}
