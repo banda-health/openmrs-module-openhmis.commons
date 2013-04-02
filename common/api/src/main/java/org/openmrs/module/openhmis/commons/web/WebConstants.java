2628f13eb021272a427d782a5f6abcb9e541ad57
Wes Brown <wes@weslandia.org>
2013-04-02 23:13:33 +0300

    Added some documentation and the module resource base folder to the WebConstants file.

--- common/api/src/main/java/org/openmrs/module/openhmis/commons/web/WebConstants.java
+++ common/api/src/main/java/org/openmrs/module/openhmis/commons/web/WebConstants.java
@@ -14,12 +14,23 @@
 package org.openmrs.module.openhmis.commons.web;
 
 public class WebConstants {
-	public static final String MODULE_BASE = "/module/openhmis/";
+	protected static final String MODULE_BASE = "/module/openhmis/";
+	protected static final String MODULE_RESOURCE_BASE = "/moduleResources/openhmis/";
 
+	/**
+	 * Adds the '.form' ending to the specified page, if it does not already exist.
+	 * @param page The page to add the form ending to.
+	 * @return The page with '.form' appended to the end.
+	 */
 	public static String formUrl(String page) {
 		return page.endsWith(".form") ? page : page + ".form";
 	}
 
+	/**
+	 * Creates the redirect url for the specified page.
+	 * @param page The page to redirect to.
+	 * @return The redirect url.
+	 */
 	public static String redirectUrl(String page) {
 		return "redirect:" + formUrl(page);
 	}
