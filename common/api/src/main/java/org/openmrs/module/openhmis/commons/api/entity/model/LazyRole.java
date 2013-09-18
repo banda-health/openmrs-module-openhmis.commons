b05f36ee4b7c89f0ba73718cdc51bb547143bd08
Wes Brown <wes@weslandia.org>
2013-09-18 09:57:54 +0300

    Created LazyRole class to allow lazy-loading a Role.  This fixes issues with Role fields being included in results from hibernate which causes Criteria.list() to return an Object array rather than just a list of the specified entity objects.

---
+++ common/api/src/main/java/org/openmrs/module/openhmis/commons/api/entity/model/LazyRole.java
@@ -0,0 +1,40 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.openhmis.commons.api.entity.model;
+
+import org.openmrs.Role;
+
+public class LazyRole extends Role {
+	public LazyRole() {
+		super();
+	}
+
+	public LazyRole(Role role) {
+		setName(role.getName());
+		setChangedBy(role.getChangedBy());
+		setChildRoles(role.getChildRoles());
+		setCreator(role.getCreator());
+		setDateChanged(role.getDateChanged());
+		setDateCreated(role.getDateCreated());
+		setDateRetired(role.getDateRetired());
+		setDescription(role.getDescription());
+		setInheritedRoles(role.getInheritedRoles());
+		setPrivileges(role.getPrivileges());
+		setRetired(role.getRetired());
+		setRetiredBy(role.getRetiredBy());
+		setRetireReason(role.getRetireReason());
+		setRole(role.getRole());
+		setUuid(role.getUuid());
+	}
+}
