cbbf99312c470475247cc9f099ee345be4d1d9d2
Wes Brown <wes@weslandia.org>
2014-10-31 11:39:06 +0300

    Added support for optional modules.     Created ModuleUtil class to test if a module is loaded.     Created "safe" class for calling Idgen methods without referencing IDGen classes.

---
+++ api/src/main/java/org/openmrs/module/openhmis/commons/api/util/ModuleUtil.java
@@ -0,0 +1,27 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * Copyright (C) OpenHMIS.  All Rights Reserved.
+ */
+package org.openmrs.module.openhmis.commons.api.util;
+
+import org.openmrs.module.ModuleFactory;
+
+/**
+ * Utility class for working with modules.
+ */
+public class ModuleUtil {
+	public static final String IDGEN_MODULE_ID = "idgen";
+	
+	public static Boolean isLoaded(String moduleId) {
+		return ModuleFactory.getStartedModuleById(moduleId) != null;
+	}
+}
