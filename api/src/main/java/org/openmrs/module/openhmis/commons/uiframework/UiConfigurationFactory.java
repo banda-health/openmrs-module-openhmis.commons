fe966edd4a282a05174afd77a392eb8f3d2b3a7d
Andrew <andrewndungu@gmail.com>
2015-11-30 10:52:30 +0300

    Code review fixes

--- api/src/main/java/org/openmrs/module/openhmis/commons/uiframework/UiConfigurationFactory.java
+++ api/src/main/java/org/openmrs/module/openhmis/commons/uiframework/UiConfigurationFactory.java
@@ -10,7 +10,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Abstract functionality for registering a UIFramework 2.x module.
+ * Abstract functionality for registering a UIFramework 2.x module. This class
+ * will ONLY load modules if the UiFramework 2.x module is loaded. It replaces
+ * use of openmrs annotations which do not check if UiFramework 2.x has been
+ * loaded.
  */
 public abstract class UiConfigurationFactory implements BeanFactoryPostProcessor {
 
@@ -22,12 +25,11 @@ public abstract class UiConfigurationFactory implements BeanFactoryPostProcessor
 	public abstract String getModuleId();
 
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		log.info("Register " + getModuleId() + " module");
-		try {
-			// load UiFramework's StandardModuleUiConfiguration class
-			Class cls = OpenmrsClassLoader.getInstance()
-			        .loadClass("org.openmrs.ui.framework.StandardModuleUiConfiguration");
-
+		log.info("Start registering " + getModuleId() + " module");
+		// checks if UiFramework's StandardModuleUiConfiguration class has been
+		// loaded.
+		Class cls = checkStandardModuleUiConfigurationClassExists();
+		if (cls != null) {
 			// get spring's bean definition builder
 			BeanDefinitionBuilder builder = BeanDefinitionBuilder.rootBeanDefinition(cls);
 
@@ -35,11 +37,23 @@ public abstract class UiConfigurationFactory implements BeanFactoryPostProcessor
 			builder.addPropertyValue("moduleId", getModuleId());
 
 			// register bean
-			((DefaultListableBeanFactory)beanFactory).registerBeanDefinition(
-			    getModuleId() + "StandardModuleUiConfiguration", builder.getBeanDefinition());
-		} catch (ClassNotFoundException ex) {
-			// StandardModuleUiConfiguration class not found!
-			log.info("Unable to register " + getModuleId() + " UI 2.x module");
+			((DefaultListableBeanFactory) beanFactory).registerBeanDefinition(
+					getModuleId() + "StandardModuleUiConfiguration", builder.getBeanDefinition());
+
+			log.info(getModuleId() + " successfully registered!");
+
+		} else {
+			log.info(getModuleId() + " not registered!");
 		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	private Class checkStandardModuleUiConfigurationClassExists() {
+		try {
+			return OpenmrsClassLoader.getInstance().loadClass("org.openmrs.ui.framework.StandardModuleUiConfiguration");
+		} catch (ClassNotFoundException ex) {
+			log.info("StandardModuleUiConfiguration class not found. UIFramework 2.x module not loaded.");
+		}
+		return null;
 	}
 }
