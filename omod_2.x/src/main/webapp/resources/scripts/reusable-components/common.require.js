3728ab8c7517f5bfb3d9f08202d53feb6d8ef10f
dubdabasoduba <dubdabasoduba@gmail.com>
2016-02-29 16:03:26 +0300

    add copyrights

--- omod_2.x/src/main/webapp/resources/scripts/reusable-components/common.require.js
+++ omod_2.x/src/main/webapp/resources/scripts/reusable-components/common.require.js
@@ -1,3 +1,18 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * Copyright (C) OpenHMIS.  All Rights Reserved.
+ *
+ */
+
 /* load common modules */
 define(['reusable-components/js/common-js', 'reusable-components/lib/restangular.min', 'reusable-components/lib/dirPagination', 'reusable-components/lib/angular-cookies.min',
     'reusable-components/models/generic.model.module', 'reusable-components/models/generic.object.model',
