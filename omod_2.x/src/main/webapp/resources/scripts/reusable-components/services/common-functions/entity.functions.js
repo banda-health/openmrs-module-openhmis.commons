4f1881203b8e00344cf2f28e8341e73839c43ce3
Andrew <andrewndungu@gmail.com>
2016-04-14 11:05:03 +0300

    core-69 Fix auto-complete search components

--- omod_2.x/src/main/webapp/resources/scripts/reusable-components/services/common-functions/entity.functions.js
+++ omod_2.x/src/main/webapp/resources/scripts/reusable-components/services/common-functions/entity.functions.js
@@ -70,4 +70,20 @@
 			return formatFields;
 		}
 	}
+
+	app.directive('ngEnter', function ($window) {
+		return function (scope, element, attrs) {
+			element.bind("keydown keypress", function (event) {
+				if (event.which === 13) {
+					scope.$apply(function () {
+
+						scope.$eval(attrs.ngEnter, {'event' : event});
+					});
+
+					event.preventDefault();
+				}
+			});
+		};
+	});
+
 })();
