f22d013182c9723f76be70e8545706b83775124c
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2016-08-12 14:15:24 +0300

    Moving the Cancel_page constant

--- omod_2.x/src/main/webapp/resources/scripts/reusable-components/constants.js
+++ omod_2.x/src/main/webapp/resources/scripts/reusable-components/constants.js
@@ -5,4 +5,3 @@ var MODULE_PRIVILEGE_SETTINGS_URL = 'module/openhmis/commons/privileges.page';
 var LOGIN_URL = ROOT_URL + 'login.htm';
 var COMMONS_RESOURCES_URL = '/' + OPENMRS_CONTEXT_PATH + '/ms/uiframework/resource/' + MODULE_PATH + '/scripts/';
 var REUSABLE_COMPONENTS_URL = '/' + OPENMRS_CONTEXT_PATH + '/moduleResources/openhmis/commons/scripts/reusable-components/';
-var CANCEL_PAGE = 'entities.page';
