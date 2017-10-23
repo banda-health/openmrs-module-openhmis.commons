a0211566f3886a956af3021d50da819aff4a6031
Kevin Burnett <kwburnett@gmail.com>
2017-10-23 15:49:23 +0300

    making changes based on Upsource code check recommendations

--- api/src/test/java/org/openmrs/module/openhmis/commons/api/TestUtil.java
+++ api/src/test/java/org/openmrs/module/openhmis/commons/api/TestUtil.java
@@ -4,47 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.OpenmrsUtil;
 
-/**
- * Created by Kevin on 7/11/2017.
- */
 public class TestUtil {
-
-	public static final String TEST_DATASETS_PROPERTIES_FILE = "test-datasets.properties";
-
-	/**
-	 * Determines the name of the proper test dataset based on what version of OpenMRS we are testing against
-	 */
-	public String getTestDatasetFilename(String testDatasetName) throws Exception {
-
-		InputStream propertiesFileStream = null;
-
-		// try to load the file if its a straight up path to the file or
-		// if its a classpath path to the file
-		if (new File(TEST_DATASETS_PROPERTIES_FILE).exists()) {
-			propertiesFileStream = new FileInputStream(TEST_DATASETS_PROPERTIES_FILE);
-		} else {
-			propertiesFileStream = getClass().getClassLoader().getResourceAsStream(TEST_DATASETS_PROPERTIES_FILE);
-			if (propertiesFileStream == null) {
-				throw new FileNotFoundException("Unable to find '" + TEST_DATASETS_PROPERTIES_FILE + "' in the classpath");
-			}
-		}
-
-		Properties props = new Properties();
-
-		OpenmrsUtil.loadProperties(props, propertiesFileStream);
-
-		if (props.getProperty(testDatasetName) == null) {
-			throw new Exception("Test dataset named " + testDatasetName + " not found in properties file");
-		}
-
-		return props.getProperty(testDatasetName);
-	}
 
 	public String getVersionedFileIfExists(String datasetFileName) {
 		String openMRSVersion = OpenmrsConstants.OPENMRS_VERSION_SHORT;
